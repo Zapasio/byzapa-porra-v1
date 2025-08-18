3965f8bcfa73bb40a9b0463c4fdecb47a65274ff
Zapa <tu-email@algo.com>
2025-08-18 17:48:31 +0200

    Añadida API key de Firebase correcta

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,10 @@
 // src/firebase.ts
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  setPersistence,
-  browserLocalPersistence,
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP8HKsR_a2hRot5QUApJiq5S_Fj4u-Y08",
+  apiKey: "AlzaSyBP8HKsR_a2hRot5QUApJiq5S_Fj4u-Y08",
   authDomain: "byzapa-porra-v1.firebaseapp.com",
   projectId: "byzapa-porra-v1",
   storageBucket: "byzapa-porra-v1.appspot.com",
@@ -18,18 +13,9 @@ const firebaseConfig = {
   measurementId: "G-P9TY8401DJ",
 };
 
-export const app = initializeApp(firebaseConfig);
+// Inicializamos Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportamos servicios que usaremos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-
-// persistencia local
-setPersistence(auth, browserLocalPersistence).catch((err) => {
-  console.error("Persistencia:", err);
-});
-
-// ayuda de diagnóstico (se ve en la consola del navegador)
-console.log("Firebase apiKey en runtime:", app.options.apiKey);
-console.log("Firebase authDomain en runtime:", app.options.authDomain);
